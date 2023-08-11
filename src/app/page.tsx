27d054fbe3b4f5084b3fb78b63e1157aec809e06
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2023-08-11 01:36:05 +0100

    Use simple string template in base page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      Sample page content
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img alt="Trigger for test api" src="/api/page1" />
-    </div>
-  )
+  return "Sample page content"
 }
