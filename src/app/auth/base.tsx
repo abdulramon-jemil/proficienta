40d29d86b5cfb7c29e5cc5aca63f21cdefae864a
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2023-08-13 01:49:42 +0100

    Rename constant

--- src/app/auth/base.tsx
+++ src/app/auth/base.tsx
@@ -1,5 +1,5 @@
-import { ENROLLMENT_PAGE } from "@/constants/pages"
+import { ENROLLMENTS_PAGE } from "@/constants/pages"
 
-export const AUTH_PAGE_DEFAULT_REDIRECT_URL = ENROLLMENT_PAGE
+export const AUTH_PAGE_DEFAULT_REDIRECT_URL = ENROLLMENTS_PAGE
 // In case you want to delay before redirecting after auth
 export const AUTH_PAGE_REDIRECT_DELAY_MS = 2000
--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -5,4 +5,4 @@ export const SIGN_UP_PAGE = "/auth/signup"
 export const SIGN_IN_PAGE = "/auth/signin"
 export const AUTH_VERIFICATION_PAGE = "/auth/verify"
 
-export const ENROLLMENT_PAGE = "/enrollments"
+export const ENROLLMENTS_PAGE = "/enrollments"
